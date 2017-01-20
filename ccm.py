4c3f2241e05dc9b94d7239339757a64d4b7c6a7d
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-01-20 15:30:20 +0900

    select func

--- ccm.py
+++ ccm.py
@@ -7,20 +7,23 @@ import matplotlib.pyplot as plt
 class Ccm:
 
     def __init__(self, time_series_data, target_data, dimension_E,
-                 points_num_L, delta_T):
+                 points_num_L, delta_T, lib_Size=0):
         self.time_series_data = time_series_data  # []
         self.target_data = target_data
         self.dimension_E = dimension_E
         self.points_num_L = points_num_L
         self.delta_T = delta_T
+        self.lib_Size = lib_Size
 
         self.start_point = delta_T * (dimension_E - 1) + 1
         self.end_point = points_num_L
         self.manifold_data = []
         self.manifold_data_num = points_num_L - (dimension_E - 1) * delta_T
+        self.selected_manifold_data = []
         self.points_distances = [
             [0 for x in xrange(self.manifold_data_num)]
             for y in xrange(self.manifold_data_num)]
+        self.selected_points_distances = []
         self.weights = []
         self.indices = []
         self.estimate_results = []
@@ -34,7 +37,23 @@ class Ccm:
                     self.time_series_data[t - i * self.delta_T])
             self.manifold_data.append(temp_manifold_data)
 
-    def find_nearest_neighbor(self):
+    def select_mnifold(self):
+        # choose later points with the number of lib_size
+        last_point_indice = self.manifold_data_num - self.lib_Size
+        selected_points_num = last_point_indice + 1
+        for i in xrange(selected_points_num):
+            self.selected_manifold_data.append(
+                self.manifold_data[i:i + self.lib_Size])
+
+    def find_nearest_neighbor_selected(self):
+        for selected_units in self.selected_manifold_data:
+            array_selected_units = np.array(selected_units)
+            temp_distance = [sum((array_selected_units[0] -
+                                  array_selected_units[i])**2)**0.5
+                             for i in range(1, self.lib_Size)]
+            self.selected_points_distances.append(temp_distance)
+
+    def find_nearest_neighbor_all(self):
         array_manifold_data = np.array(self.manifold_data)
         for i in xrange(self.manifold_data_num):
             for j in xrange(i, self.manifold_data_num):
@@ -109,7 +128,7 @@ class Ccm:
         self.correlation = p_value
 
 
-def main(start_size, end_size, interval):
+def main_all_points(start_size, end_size, interval):
     correlations = []
     # read data
     kyoto_nov, osaka_nov, kobe_nov = output_data()
@@ -132,19 +151,56 @@ def main(start_size, end_size, interval):
         # temp_ccm = Ccm(temp_kyoto, temp_tokyo, dimension_E=2,
         #                points_num_L=temp_len, delta_T=1)
         temp_ccm.create_manifold()
-        temp_ccm.find_nearest_neighbor()
+        temp_ccm.find_nearest_neighbor_all()
         temp_ccm.create_weights2()
         temp_ccm.estimate_y()
         temp_ccm.compute_correlation()
         correlations.append(temp_ccm.correlation)
     xs = range(start_size, end_size, interval)
     f, ax = plt.subplots()
-    ax.set_title('Kyoto->Osaka')
+    ax.set_title('Osaka->Kyoto')
     ax.set_xlabel('Library Size')
     ax.set_ylabel('Coefficient')
-    line, = ax.plot(xs, correlations, 'r', lw=1, label='Kyoto->Osaka')
+    line, = ax.plot(xs, correlations, 'r', lw=1, label='Osaka->Kyoto')
+    plt.show()
+
+
+def main_selected_points(start_size, end_size, interval):
+    correlations = []
+    # read data
+    kyoto_nov, osaka_nov, kobe_nov = output_data()
+    # tokyo_nov = data[0][0]
+    kyoto_nov_temp = []
+    osaka_nov_temp = []
+    # tokyo_nov_temp = []
+    for i in kyoto_nov:
+        kyoto_nov_temp.append(float(i[5]))
+    for i in osaka_nov:
+        osaka_nov_temp.append(float(i[5]))
+
+    for i in xrange(start_size, end_size, interval):
+        # temp_tokyo = tokyo_nov_temp[:i]
+        temp_len = len(kyoto_nov_temp)
+        temp_ccm = Ccm(kyoto_nov_temp, osaka_nov_temp, dimension_E=2,
+                       points_num_L=temp_len, delta_T=1, lib_Size=i)
+        # temp_ccm = Ccm(temp_kyoto, temp_tokyo, dimension_E=2,
+        #                points_num_L=temp_len, delta_T=1)
+        temp_ccm.create_manifold()
+        temp_ccm.select_mnifold()
+        temp_ccm.find_nearest_neighbor_selected()
+        temp_ccm.create_weights2()
+        temp_ccm.estimate_y()
+        temp_ccm.compute_correlation()
+        correlations.append(temp_ccm.correlation)
+    xs = range(start_size, end_size, interval)
+    f, ax = plt.subplots()
+    ax.set_title('Osaka->Kyoto')
+    ax.set_xlabel('Library Size')
+    ax.set_ylabel('Coefficient')
+    line, = ax.plot(xs, correlations, 'r', lw=1, label='Osaka->Kyoto')
     plt.show()
 
 
 if __name__ == '__main__':
-    main(20, 280, 1)
+    main_all_points(25, 60, 1)
+    # main_selected_points(25, 60, 1)
